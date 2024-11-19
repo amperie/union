63afcd321e5b3e919d3d11037beed06b6148a435
amperie <amperie@yahoo.com>
2024-11-19 12:41:27 -0800

    r

---
+++ actor-demo/actor.py
@@ -0,0 +1,31 @@
+import os
+
+from flytekit import workflow, Resources, ImageSpec
+from union.actor import ActorEnvironment
+
+image = ImageSpec(
+    registry=os.environ.get("DOCKER_REGISTRY", None),
+    packages=["union"],
+)
+
+actor = ActorEnvironment(
+    name="my-actor",
+    replica_count=1,
+    ttl_seconds=30,
+    requests=Resources(
+        cpu="2",
+        mem="300Mi",
+    ),
+    container_image=image,
+)
+
+
+@actor.task
+def say_hello() -> str:
+    print ("hello")
+    return "hello"
+
+
+@workflow
+def actor_wf():
+    say_hello()
